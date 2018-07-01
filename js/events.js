74fac461fe90bdb1824ebefb2e5ed7a6966f4d56
Martin Valkov <mvalkov60@gmail.com>
2018-07-01 02:46:23 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -8,14 +8,14 @@ function frameIt(){
   $("img").addClass("tasty");
 }
 
-function pressIt({
+function pressIt(){
   $("form").on("keydown",function(key){
     if (key.which ===71){
       alert('You have pressed the mfn G key');
     }
   })
   
-});
+}
 
 
 function sumbitIt()  {
