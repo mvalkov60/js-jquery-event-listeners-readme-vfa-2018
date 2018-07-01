f33651712b55e20fed2d5b0ee9fe83cf9b80a440
Martin Valkov <mvalkov60@gmail.com>
2018-07-01 02:46:34 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,30 @@
 //define functions here
 
+function getIt(){
+  alert('Hey!');
+}
+
+function frameIt(){
+  $("img").addClass("tasty");
+}
+
+function pressIt(){
+  $("form").on("keydown",function(key){
+    if (key.which ===71){
+      alert('You have pressed the mfn G key');
+    }
+  })
+  
+}
+
+
+function sumbitIt()  {
+  $("form").on("submit",function() {
+    alert('Your form is going to be submitted now.');
+  });
+}
+
 $(document).ready(function(){
-
-// call functions here
-
-});
+  $("p").on("click",getIt());
+  $("img").on("load",frameIt);
+})
